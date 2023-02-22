d7fb130390f003d01243260792daadf365762d8d
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-02-22 08:26:57 -0600

    updated length function

--- Develop/script.js
+++ Develop/script.js
@@ -1,40 +1,29 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var charLetters = "abcdefghijklmnopqrstuvwxyz"
-var charLettersUpper = charLetters.toUpperCase();
-var charNumber = "1234567890"
-var charSpecial = " ~!@#$%^&*()_+{}[]:;'|\<>?,./"
-console.log(charLettersUpper)
+var charLettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charLettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var charNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var charSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "[", "]", ":", ";", "'", "|", "<", ">", "?", ",", ".", "/"];
+var newPassword = "";
 // Function begins
 function writePassword() {
-// Function prompts
+// Length
   var length = window.prompt("How long would you like your password? Must be between 8 and 128 characters.");
     if (!length) {
-      alert("Please choose a number between 8 and 128.")
+      alert("Please choose a number between 8 and 128.");
+
     } else if (length < 8 || length > 128)  {
-      var length = window.prompt("The number you choose must be between 8 and 128 characters")
-    }
-  
+      var length = window.prompt("The number you choose must be between 8 and 128 characters");
 
-  var upper = window.confirm("Would you like uppercase letters in your password?")
+    } else {
+      var upper = window.confirm("Would you like uppercase letters in your password?");
+      var lower = window.confirm("Would you like lowercase letters in your password?");
+      var numbers = window.confirm("Would you like numbers in your password?");
+      var special = window.confirm("Would you like special characters in your password?");
+     }
 
-
-  var lower = window.confirm("Would you like lowercase letters in your password?")
-
-
-  var numbers = window.confirm("Would you like numbers in your password?")
-
-
-  var special = window.confirm("Would you like special characters in your password?")
-
-
-  var newPassword = "";
-  while (length <= 8 || length >= 128) {
-    if (!length) {
-      alert("Please choose a number between 8 and 128.")
-    }
-  }
 console.log(length)
+console.log(newPassword)
 
 
 
