3246a055ed71831b81285ba8017d7e7479ff6b2e
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-02-22 15:57:49 -0600

    Added randomization - has length bug

--- Develop/script.js
+++ Develop/script.js
@@ -4,41 +4,48 @@ var charLettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "
 var charLettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var charNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var charSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "[", "]", ":", ";", "'", "|", "<", ">", "?", ",", ".", "/"];
-var newPassword = "";
+var concatPassword = "";
 // Function begins
 function writePassword() {
 // Length
-  var length = window.prompt("How long would you like your password? Must be between 8 and 128 characters.");
-    if (!length) {
-      alert("Please choose a number between 8 and 128.");
+  var setLength = window.prompt("How long would you like your password? Must be between 8 and 128 characters.");
+    if (!setLength) {
+      window.prompt("Please choose a number between 8 and 128.");
 
-    } else if (length < 8 || length > 128)  {
+    } else if (setLength < 8 || setLength > 128)  {
       var length = window.prompt("The number you choose must be between 8 and 128 characters");
-
+//character selection confirmation
     } else {
-      var upper = window.confirm("Would you like uppercase letters in your password?");
-      var lower = window.confirm("Would you like lowercase letters in your password?");
-      var numbers = window.confirm("Would you like numbers in your password?");
-      var special = window.confirm("Would you like special characters in your password?");
+      var addUpper = window.confirm("Would you like uppercase letters in your password?");
+      var addLower = window.confirm("Would you like lowercase letters in your password?");
+      var addNumbers = window.confirm("Would you like numbers in your password?");
+      var addSpecial = window.confirm("Would you like special characters in your password?");
      };
 
-     //one char type selected
-     if (upper) {
-      newPassword = newPassword.concat(charLettersUpper)
-     } else if (lower) {
-      newPassword = newPassword.concat(charLettersLower)
-     } else if (numbers) {
-      newPassword = newPassword.concat(charNumber)
-     } else if (special) {
-      newPassword = newPassword.concat(charSpecial)
+//character type selection
+     if (addUpper) {
+      concatPassword = concatPassword.concat(charLettersUpper);
      }
+     if (addLower) {
+      concatPassword = concatPassword.concat(charLettersLower);
+     }
+     if (addNumbers) {
+      concatPassword = concatPassword.concat(charNumber);
+     }
+     if (addSpecial) {
+      concatPassword = concatPassword.concat(charSpecial);
+     }
+     
+//Randomization
+     var newPassword = "";
 
-
+     for (var i = 0; i < length; i++);
+      var newPassword = newPassword + concatPassword[Math.floor(Math.random() * concatPassword.length)];
 
 //test console logs
-console.log(length)
-console.log(newPassword)
-
+console.log(setLength);
+console.log(newPassword);
+console.log(concatPassword)
 
 
 
