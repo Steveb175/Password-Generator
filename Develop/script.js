ade57a161046c2056567539045b028ad827b8fba
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-02-26 09:49:37 -0600

    Fixed bugs, password is now successfully generating and inserting into HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,183 @@
-// Assignment Code
+// Arrays
 var generateBtn = document.querySelector("#generate");
-var charLettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var charLettersUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var charLettersLower = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var charLettersUpper = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var charNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var charSpecial = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "{", "}", "[", "]", ":", ";", "'", "|", "<", ">", "?", ",", ".", "/"];
+var charSpecial = [
+  "~",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "+",
+  "{",
+  "}",
+  "[",
+  "]",
+  ":",
+  ";",
+  "'",
+  "|",
+  "<",
+  ">",
+  "?",
+  ",",
+  ".",
+  "/",
+];
+
+// Variables
 var concatPassword = "";
+var setLength = "";
+var addUpper;
+var addLower;
+var addNumbers;
+var addSpecial;
+
 // Function begins
-function writePassword() {
-// Length
-  var setLength = window.prompt("How long would you like your password? Must be between 8 and 128 characters.");
-    if (!setLength) {
-      window.prompt("Please choose a number between 8 and 128.");
+function generatePassword() {
+  // Length
+  var setLength = window.prompt(
+    "How long would you like your password? Must be between 8 and 128 characters."
+  );
+  setLength = parseInt(setLength);
 
-    } else if (setLength < 8 || setLength > 128)  {
-      var length = window.prompt("The number you choose must be between 8 and 128 characters");
-//character selection confirmation
-    } else {
-      var addUpper = window.confirm("Would you like uppercase letters in your password?");
-      var addLower = window.confirm("Would you like lowercase letters in your password?");
-      var addNumbers = window.confirm("Would you like numbers in your password?");
-      var addSpecial = window.confirm("Would you like special characters in your password?");
-        while(addUpper === false && addLower === false && addNumbers === false && addSpecial === false) {
-          window.alert("Please choose at least one variable.")
-          var addUpper = window.confirm("Would you like uppercase letters in your password?");
-          var addLower = window.confirm("Would you like lowercase letters in your password?");
-          var addNumbers = window.confirm("Would you like numbers in your password?");
-          var addSpecial = window.confirm("Would you like special characters in your password?");
-        }
-     };
+  if (!setLength) {
+    window.prompt("Please choose a number between 8 and 128.");
+  } else if (setLength < 8 || setLength > 128) {
+    var setLength = window.prompt(
+      "The number you choose must be between 8 and 128 characters"
+    );
+    //character selection confirmation
+  } else {
+    var addUpper = window.confirm(
+      "Would you like uppercase letters in your password?"
+    );
+    var addLower = window.confirm(
+      "Would you like lowercase letters in your password?"
+    );
+    var addNumbers = window.confirm("Would you like numbers in your password?");
+    var addSpecial = window.confirm(
+      "Would you like special characters in your password?"
+    );
+    // if all false, ask questions again
+    while (
+      addUpper === false &&
+      addLower === false &&
+      addNumbers === false &&
+      addSpecial === false
+    ) {
+      window.alert("Please choose at least one variable.");
+      var addUpper = window.confirm(
+        "Would you like uppercase letters in your password?"
+      );
+      var addLower = window.confirm(
+        "Would you like lowercase letters in your password?"
+      );
+      var addNumbers = window.confirm(
+        "Would you like numbers in your password?"
+      );
+      var addSpecial = window.confirm(
+        "Would you like special characters in your password?"
+      );
+    }
+  }
 
-//character type selection
-     if (addUpper) {
-      concatPassword = concatPassword.concat(charLettersUpper);
-     }
-     if (addLower) {
-      concatPassword = concatPassword.concat(charLettersLower);
-     }
-     if (addNumbers) {
-      concatPassword = concatPassword.concat(charNumber);
-     }
-     if (addSpecial) {
-      concatPassword = concatPassword.concat(charSpecial);
-     }
-     
-//Randomization
-     var newPassword = [];
+  // character type selection
+  var concatPassword = [];
+  if (addUpper) {
+    concatPassword = concatPassword.concat(charLettersUpper);
+  }
+  if (addLower) {
+    concatPassword = concatPassword.concat(charLettersLower);
+  }
+  if (addNumbers) {
+    concatPassword = concatPassword.concat(charNumber);
+  }
+  if (addSpecial) {
+    concatPassword = concatPassword.concat(charSpecial);
+  }
 
-     for (var i = 0; i < length; i++);
-      newPassword = newPassword + concatPassword[Math.floor(Math.random() * concatPassword.length)];
+  // Joining the concat arrays
+  var joinedConcat = concatPassword.join("");
 
-//test console logs
-console.log(setLength);
-console.log(newPassword);
-console.log(concatPassword)
+  // Randomization
+  var newPassword = "";
 
-
-
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
- 
-  passwordText.value = password;
-
+  for (let i = 0; i < setLength; i++) {
+    newPassword +=
+      joinedConcat[Math.floor(Math.random() * joinedConcat.length)];
+  }
+  return newPassword;
 }
 
+// Writes password in HTML
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
